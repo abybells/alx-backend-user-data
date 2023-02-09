b4a1aaf42b49bfa76f33547dd974968d18291ea2
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2023-02-09 15:28:42 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,7 +33,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
- @app.errorhandler(401)
+@app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler.
     """
